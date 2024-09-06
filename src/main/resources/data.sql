95dd61d5642a7f289bfd9e1d099dc33a5cfe11e1
loutix <62959342+loutix@users.noreply.github.com>
2024-09-06 18:59:15 +0200

    adapt to postgree

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,82 +1,67 @@
-# DELETE
-# FROM friendships;
-# DELETE
-# FROM transactions;
-# DELETE
-# FROM bank_accounts;
-# DELETE
-# FROM users;
-
+-- Supprimer les tables si elles existent
 DROP TABLE IF EXISTS friendships;
 DROP TABLE IF EXISTS transactions;
 DROP TABLE IF EXISTS bank_accounts;
 DROP TABLE IF EXISTS users;
 
-
-CREATE TABLE users
-(
-    id         INT AUTO_INCREMENT PRIMARY KEY,
+-- Créer la table users
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(250) NOT NULL,
-    last_name  VARCHAR(250) NOT NULL,
-    email      VARCHAR(250) NOT NULL,
-    password   VARCHAR(250) NOT NULL
+    last_name VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NOT NULL,
+    password VARCHAR(250) NOT NULL
 );
 
+-- Insérer des données dans la table users
+INSERT INTO users (first_name, last_name, email, password) VALUES
+    ('John', 'Dupont', '1@gmail.com', '$2a$10$rgVyJt18/S/QFuDoL1oqoOOYPjLvyN2eLJDurWgOzeD/khrqLvjAK'),
+    ('Henry', 'Charles', '2@gmail.com', '$2a$10$rgVyJt18/S/QFuDoL1oqoOOYPjLvyN2eLJDurWgOzeD/khrqLvjAK'),
+    ('Amelie', 'Boudea', '3@gmail.com', '$2a$10$rgVyJt18/S/QFuDoL1oqoOOYPjLvyN2eLJDurWgOzeD/khrqLvjAK'),
+    ('Loïc', 'Dubrulle', 'loutix@hotmail.com', '$2a$10$rgVyJt18/S/QFuDoL1oqoOOYPjLvyN2eLJDurWgOzeD/khrqLvjAK');
 
-INSERT INTO users (first_name, last_name, email, password)
-VALUES ('John', 'Dupont', '1@gmail.com', '$2a$10$rgVyJt18/S/QFuDoL1oqoOOYPjLvyN2eLJDurWgOzeD/khrqLvjAK'),
-       ('Henry', 'Charles', '2@gmail.com', '$2a$10$rgVyJt18/S/QFuDoL1oqoOOYPjLvyN2eLJDurWgOzeD/khrqLvjAK'),
-       ('Amelie', 'Boudea', '3@gmail.com', '$2a$10$rgVyJt18/S/QFuDoL1oqoOOYPjLvyN2eLJDurWgOzeD/khrqLvjAK'),
-       ('Loïc', 'Dubrulle', 'loutix@hotmail.com', '$2a$10$rgVyJt18/S/QFuDoL1oqoOOYPjLvyN2eLJDurWgOzeD/khrqLvjAK');
-
-CREATE TABLE bank_accounts
-(
-    id      INT AUTO_INCREMENT PRIMARY KEY,
+-- Créer la table bank_accounts
+CREATE TABLE bank_accounts (
+    id SERIAL PRIMARY KEY,
     balance DECIMAL(10, 2) DEFAULT 0.00,
-    user_id INT,
-    iban    VARCHAR(50),
-    CONSTRAINT fk_user_bank_account FOREIGN KEY (user_id) REFERENCES users (id)
+    user_id INTEGER REFERENCES users(id),
+    iban VARCHAR(50)
 );
 
+-- Insérer des données dans la table bank_accounts
+INSERT INTO bank_accounts (balance, user_id, iban) VALUES
+    (100, 1, 'FR12345678901234567890123456'),
+    (200, 2, 'FRABCDEF1234567890123456789'),
+    (300, 3, 'FRABCDEF1234567890123456789'),
+    (400, 4, 'FR12345678901234567890123456');
 
-INSERT INTO bank_accounts (balance, user_id, iban)
-VALUES (100, 1, "FR12345678901234567890123456"),
-       (200, 2, "FRABCDEF1234567890123456789"),
-       (300, 3, "FRABCDEF1234567890123456789"),
-       (400, 4, "FR12345678901234567890123456");
-
-
-CREATE TABLE friendships
-(
-    id        INT AUTO_INCREMENT PRIMARY KEY,
-    user_id   INT,
-    friend_id INT,
-    CONSTRAINT fk_user_friendship FOREIGN KEY (user_id) REFERENCES users (id),
-    CONSTRAINT fk_friend_friendship FOREIGN KEY (friend_id) REFERENCES users (id)
+-- Créer la table friendships
+CREATE TABLE friendships (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    friend_id INTEGER REFERENCES users(id)
 );
 
+-- Insérer des données dans la table friendships
+INSERT INTO friendships (user_id, friend_id) VALUES
+    (1, 2),
+    (4, 1),
+    (4, 3);
 
-INSERT INTO friendships (user_id, friend_id)
-VALUES (1, 2),
-       (4, 1),
-       (4, 3);
-
-
-CREATE TABLE transactions
-(
-    id               INT AUTO_INCREMENT PRIMARY KEY,
-    amount           DECIMAL(10, 2) DEFAULT 0.00,
-    description      VARCHAR(50),
+-- Créer la table transactions
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    amount DECIMAL(10, 2) DEFAULT 0.00,
+    description VARCHAR(50),
     transaction_type VARCHAR(25),
-    date             TIMESTAMP      DEFAULT CURRENT_TIMESTAMP,
-    bank_account_id  INT,
-    friend_bank_id   INT,
-    CONSTRAINT fk_bank_account_transaction FOREIGN KEY (bank_account_id) REFERENCES bank_accounts (id)
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    bank_account_id INTEGER REFERENCES bank_accounts(id),
+    friend_bank_id INTEGER
 );
 
-
-INSERT INTO transactions (amount, description, transaction_type, bank_account_id, friend_bank_id, date)
-VALUES (100, "cadeau de noël", "DEBIT", 1, 4, DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 124 MINUTE)),
-       (100, "cadeau de noël", "CREDIT", 4, 1, DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 124 MINUTE)),
-       (200, "repas", "DEBIT", 4, 2, DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 3625 MINUTE)),
-       (200, "repas", "CREDIT", 2, 4, DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 3625 MINUTE));
+-- Insérer des données dans la table transactions
+INSERT INTO transactions (amount, description, transaction_type, bank_account_id, friend_bank_id, date) VALUES
+    (100, 'cadeau de noël', 'DEBIT', 1, 4, NOW() - INTERVAL '124 MINUTE'),
+    (100, 'cadeau de noël', 'CREDIT', 4, 1, NOW() - INTERVAL '124 MINUTE'),
+    (200, 'repas', 'DEBIT', 4, 2, NOW() - INTERVAL '3625 MINUTE'),
+    (200, 'repas', 'CREDIT', 2, 4, NOW() - INTERVAL '3625 MINUTE');
